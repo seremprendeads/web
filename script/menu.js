8bff10e119c296f1e878edad2d13e4edb87c91db
Leo <seremprendeads@gmail.com>
2025-08-12 15:42:53 -0300

    Delete script directory

--- script/menu.js
+++
@@ -1,9 +0,0 @@
- //menu
-  // Mobile Menu Toggle
-  function toggleMenu() {
-    const mobileMenu = document.getElementById('mobileMenu');
-    const hamburger = document.querySelector('.hamburger');
-    
-    mobileMenu.classList.toggle('hidden');
-    hamburger.classList.toggle('active');
-}
